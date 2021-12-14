ab15b2b1cadc5086bb6959cdf93b11d609d201d6
fady gamil mahrous masoud <1700961@eng.asu.edu.eg>
2021-12-14 21:36:30 +0200

    structure the project architecture

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 //! import express
 const express = require("express");
 
+//! import passport configuration setup from /services/passport
+require("./services/passport"); // we need to only execute this requrired file but we don't export anything from it
+
 //! Configure and fire express app
 const app = express();
 
-app.get("/", (req, res, next) => {
-	res.json("Running");
-});
+//! require/import auth routes and chain it to main express app
+const authRoutes = require("./routes/auth");
 
-// development env port = 5000 , production port = "From Heruko"
+//! chain all routes to app
+app.use(authRoutes);
+
+//! development env port = 5000 , production port = "From Azure"
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
